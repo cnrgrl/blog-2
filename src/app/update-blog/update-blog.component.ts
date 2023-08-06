e67a37ee66c074b4a765948cb7ab1e0f34f61efc
caner <66187631+cnrgrl@users.noreply.github.com>
2023-08-06 16:06:32 +0200

    update page created

--- src/app/update-blog/update-blog.component.ts
+++ src/app/update-blog/update-blog.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {BlogService} from "../services/blog.service";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-update-blog',
-  templateUrl: './update-blog.component.html',
-  styleUrls: ['./update-blog.component.css']
+  selector: "app-update-blog",
+  templateUrl: "./update-blog.component.html",
+  styleUrls: ["./update-blog.component.css"],
 })
 export class UpdateBlogComponent implements OnInit {
+  title: string = "";
+  body: string = "";
+  blogid: any;
+  blogObj: any;
 
-  constructor() { }
+  form = new FormGroup({
+    title: new FormControl(null, [Validators.required]),
+    body: new FormControl(null, [Validators.required]),
+  });
+  constructor(
+    private blogService: BlogService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.blogid = this.route.snapshot.paramMap.get("blogId");
+    this.blogService.getPost(this.blogid).subscribe((res) => {
+      this.blogObj = res;
+      this.form.patchValue({
+        title: this.blogObj?.title,
+        body: this.blogObj?.body,
+      });
+    });
   }
-
+  onSubmit() {
+    const request = {
+      title: this.form.get("title")?.value,
+      body: this.form.get("body")?.value,
+      imageId: this.blogObj.imageId,
+      userId: this.blogObj.userId,
+    };
+    this.blogService.UpdatePosts(this.blogObj.id, request).subscribe((res) => {
+      console.log(res);
+      this.router.navigateByUrl("home");
+    });
+  }
 }
